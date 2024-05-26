4d2f1494456c7af11449f6237d4077e34849f354
martoff <martoff@ukr.net>
2024-05-26 14:30:58 +0300

    Update home_work10_task1.py

--- home_work10_task1.py
+++ home_work10_task1.py
@@ -5,18 +5,18 @@ model = pulp.LpProblem("MaxProductivity", pulp.LpMaximize)
 
 # Визначення змінних
 # Кількість продукту "Лимонад"
-A = pulp.LpVariable('Limonad', lowBound=0, cat='Integer')
+A = pulp.LpVariable('Limonade', lowBound=0, cat='Integer')
 # Кількість продукту "Фруктовий сік"
-B = pulp.LpVariable('FructJuice', lowBound=0,  cat='Integer')
+B = pulp.LpVariable('Fruit_Juice', lowBound=0,  cat='Integer')
 
 # Функція цілі (Максимізація виробницства)
-model += A + B, "Productivity"
+model += A + B, "Total_Production"
 
 # Додавання обмежень
 model += 2*A + 1*B <= 100, "Water"  # Вода
-model += 1*A+0*B <= 50, "Sugar"  # Сахар
-model += 1*A+0*B <= 30, "LimonJuice"  # Лімоний сік
-model += 0*A+1*B <= 40, "FructPure"  # Фруктове пюре
+model += 1*A <= 50, "Sugar"  # Сахар
+model += 1*A <= 30, "Limon_Juice"  # Лімоний сік
+model += 2*B <= 40, "Fruit_Pure"  # Фруктове пюре
 
 # Розв'язання моделі
 model.solve()
